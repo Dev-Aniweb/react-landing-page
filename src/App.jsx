269dcb2fa0d83597134595bf40732ac22702dd64
Dev-Aniweb <anilkumarve51@gmail.com>
2026-01-13 21:54:15 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import React from 'react'
-
+import Navbar from './components/Navbar'
 const App = () => {
   return (
     <div>
-           <h1>Build Modern Web Experiences with React</h1>
-      <p>
-        Create fast, scalable, and interactive user interfaces using React.
-        Our solution helps you build responsive web applications with clean
-        code, reusable components, and seamless performance across all devices.
-      </p>
-      <button>Get Started</button>
+       <Navbar/>
     </div>
   )
 }
